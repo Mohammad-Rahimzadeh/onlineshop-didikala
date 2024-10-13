c9e94a6e7af02b90ca46d69cca3f9e8085a9efda
Mohammad Rahimzadeh <mhmmd.r_7910@yahoo.com>
2024-10-14 01:55:04 +0330

    Edit STATIC

--- didikala/settings.py
+++ didikala/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
 
 # Default primary key field type
