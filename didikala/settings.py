0a0b1a4b68aac0061b1935f7b1ef50e336a9b981
Mohammad Rahimzadeh <mhmmd.r_7910@yahoo.com>
2024-10-14 01:50:52 +0330

    Edit STATIC_URL

--- didikala/settings.py
+++ didikala/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
